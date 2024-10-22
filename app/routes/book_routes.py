1e5ee93d3c96f359d3faad4398d5d70794ccd9d3
shiyinxi <shiyinxi@gmail.com>
2024-10-21 20:22:40 -0700

    04 completed

--- app/routes/book_routes.py
+++ app/routes/book_routes.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, abort, make_response, request
+from flask import Blueprint, abort, make_response, request, Response
 from app.models.book import Book
 from ..db import db
 # from ..models.book import books
@@ -39,40 +39,47 @@ def get_all_books():
         )
     return books_response
 
-# @books_bp.get("")
-# def get_all_books():
-#     books_response = []
-#     for book in books:
-#         books_response.append(
-#             {
-#                 "id": book.id,
-#                 "title": book.title,
-#                 "description": book.description
-#             }
-#         )
-#     return books_response
-
-# @books_bp.get("/<book_id>")
-# def get_one_book(book_id):
-#     book = validate_book(book_id)
+@books_bp.get("/<book_id>")
+def get_one_book(book_id):
+    book = validate_book(book_id)
     
-#     return {
-#         "id" : book.id,
-#         "title" : book.title,
-#         "description" : book.description
-#     }
+    return {
+        "id" : book.id,
+        "title" : book.title,
+        "description" : book.description
+    }
 
-# def validate_book(book_id):
-#     try: 
-#         book_id = int(book_id)
-#     except:
-#         response = {"message": f"book {book_id} invalid"}
-#         abort(make_response(response, 400))
+def validate_book(book_id):
+    try: 
+        book_id = int(book_id)
+    except:
+        response = {"message": f"book {book_id} invalid"}
+        abort(make_response(response, 400))
     
-#     for book in books:
-#         if book.id == book_id:
-#             return book
-    
-#     response = {"message": f"book {book_id} not found"}
-#     abort(make_response(response, 404))
+    query = db.select(Book).where(Book.id == book_id)
+    book = db.session.scalar(query)
 
+    if not book:
+        response = {"message": f"book {book_id} not found"}
+        abort(make_response(response, 404))
+
+    return book
+
+@books_bp.put("/<book_id>")
+def update_book(book_id):
+    book = validate_book(book_id)
+    request_body = request.get_json()
+
+    book.title = request_body["title"]
+    book.description = request_body["description"]
+    db.session.commit()
+
+    return Response(status=204, mimetype="application/json")
+
+@books_bp.delete("/<book_id>")
+def delete_book(book_id):
+    book = validate_book(book_id)
+    db.session.delete(book)
+    db.session.commit()
+
+    return Response(status=204, mimetype="application/json")
